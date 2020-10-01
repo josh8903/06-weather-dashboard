90f40b06cb4e2aed8a216d438c526a4a6a7dae4f
Josh8903 <thorngren87@gmail.com>
2020-10-01 17:59:32 -0500

    search pulls API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
+var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?q=",
+    apiKey = "&appid=6cc8bbd0f9c3b88ec510c02440bb3c5a";
+
+
+
 // on click search button
 $("#search").click(function() {
     event.preventDefault();
     var searchTerm = $("input").val()
     localStorage.setItem("searchTerm", searchTerm);
     $("#cities").prepend('<button id="cityButton" type="button" class="btn btn-outline-dark">' + searchTerm + '</button>');
+    $.ajax({
+        type: "get",
+        url: queryUrl + searchTerm + apiKey,
+        success: function(response) {
+            console.log(response);
+        }
+    });
 });
 
 // load last city on page load
